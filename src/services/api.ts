7928d514ccc5ef8fc615cd09032b9116eaa5dc3f
IrfanAqila31 <irfanaqila31@gmail.com>
2026-06-16 21:41:45 +0800

    ganti baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://dummyjson.com',
+  baseURL: 'https://long-tires-report.loca.lt/api',
   headers: {
     'Content-Type': 'application/json',
   },
