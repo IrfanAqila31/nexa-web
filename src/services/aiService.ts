f4bbc20a0da4da446229e4b2e4e11bec4f178bb4
IrfanAqila31 <irfanaqila31@gmail.com>
2026-06-24 14:58:37 +0800

    mengubah get ke post lagi

--- src/services/aiService.ts
+++ src/services/aiService.ts
@@ -3,8 +3,8 @@ import api from './api'
 export const aiService = {
   //untuk meminta AI membuat caption berdasarkan ID ClipCard
   async generateCaption(clipcardId: string) {
-    // mengggunakan POST sesuai dokumentasi AI 
-    const respon = await api.get(`/ai/generate-caption/${clipcardId}`)
+    // mengggunakan POST sesuai dokumentasi AI
+    const respon = await api.post(`/ai/generate-caption/${clipcardId}`, {})
     return respon.data
-  }
+  },
 }
