7a6ea63dbc9b725a33757a4fd1b42ea92c0aa621
IrfanAqila31 <irfanaqila31@gmail.com>
2026-06-24 14:51:25 +0800

    menggubah alamat endpoint

--- src/services/aiService.ts
+++ src/services/aiService.ts
@@ -4,7 +4,7 @@ export const aiService = {
   //untuk meminta AI membuat caption berdasarkan ID ClipCard
   async generateCaption(clipcardId: string) {
     // mengggunakan POST sesuai dokumentasi AI 
-    const respon = await api.post(`/ai/caption/${clipcardId}`)
+    const respon = await api.post(`/ai/generate-caption/${clipcardId}`)
     return respon.data
   }
 }
